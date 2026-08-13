f8a7117cad6c74c92cd5d4b16903c7e148ecee27
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-08-13 20:54:53 +0530

    chore: remove temporary test controller after repository layer verification

--- backend/src/main/java/com/agrinexus/backend/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.agrinexus.backend.repository;
-
-import com.agrinexus.backend.model.TestDocument;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TestRepository extends MongoRepository<TestDocument, String> {
-}
